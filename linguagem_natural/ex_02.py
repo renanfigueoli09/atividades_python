6a30edf5bb0318b638e65cf3181f0814e4805a0a
rfdoliveira@ogmaster.local <rfdoliveira@ogmaster.local>
2023-08-22 10:53:38 -0300

    ex02

--- linguagem_natural/ex_02.py
+++ linguagem_natural/ex_02.py
@@ -3,13 +3,13 @@ a = 1103515245
 c = 12345
 m = 2**31
 
-def gerar_numero_aleatorio(seed):
+def num_ale(seed):
     seed = (a * seed + c) % m
     return seed
 
 nums = []
 for _ in range(100):
-    num = gerar_numero_aleatorio(num)
+    num = num_ale(num)
     numero_aleatorio = num % 1000 + 1
     nums.append(numero_aleatorio)
 
