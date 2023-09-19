d3e7c2569d26762c926f71cdad7083069e4c37ba
renanfigueoli09 <renanfigueoli@outlok.com>
2023-09-19 15:19:05 -0300

    NumPy

---
+++ linguagem_natural/NumPy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# m = np.array([1,2,3,4,5,6,7,8,9,10],dtype=np.float32)
+# print(m)
+
+# array_numpy = np.random.randint(0, 1, size=(300, 200, 3), dtype=np.uint8)
+# print(array_numpy)
+
+notas = np.random.randint(1, 11, size=(20, 3))
+print(notas)
